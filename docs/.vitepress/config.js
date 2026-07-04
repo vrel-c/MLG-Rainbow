6d398608ab66b3ccb041fad3631eb0b88feed21f
vrel-c <sailara.pokemongo@gmail.com>
2026-07-04 14:01:41 +0900

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,25 @@
+export default {
+  title: "MLG Rainbow",
+  base: '/MLG-Rainbow/',
+  description: "Unity向けカスタムシェーダー「MLG Rainbow」のオンラインマニュアルです。",
+  locales: {
+    root: {
+      label: '日本語',
+      lang: 'ja',
+      themeConfig: {
+        siteTitle: "MLG Rainbow",
+        sidebar: [
+          { text: '概要（トップ）', link: '/' },
+          { text: '導入方法 [WIP]' },
+          { text: 'Lite版について [WIP]' },
+          { text: '通常版について [WIP]' },
+          { text: '権利表記等 [WIP]' },
+          {
+            text: '',
+            items: [{text: 'ver1.0.0'}]
+          }
+        ]
+      }
+    }
+  }
+}
